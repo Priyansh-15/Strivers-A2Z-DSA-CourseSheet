131e8110f64124ca36e0c13a4eb463c306ae97ff
Priyansh Sharma <77832407+Priyansh-15@users.noreply.github.com>
2023-02-21 17:07:26 +0530

    Create Data_Types.cpp

---
+++ Step 1/Step 1.1/Data_Types.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        // your code here
+        if(str=="Character")
+            return 1;
+        if(str=="Integer")
+            return 4;
+        if(str=="Long")
+            return 8;
+        if(str=="Float")
+            return 4;
+        if(str=="Double")
+            return 8;
+    }
+};
