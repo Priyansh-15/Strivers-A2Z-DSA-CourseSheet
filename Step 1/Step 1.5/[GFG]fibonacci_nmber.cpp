7a8b21eaf5ff1160d0ee09ab5472c3205999d063
Priyansh Sharma <77832407+Priyansh-15@users.noreply.github.com>
2023-03-10 10:56:24 +0530

    Create [GFG]fibonacci_nmber.cpp

---
+++ Step 1/Step 1.5/[GFG]fibonacci_nmber.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+        //code here
+        if(n==1)
+            return {1};
+        if(n==2)
+            return {1,1};
+        vector<long long>ps=printFibb(n-1);
+        ps.push_back(ps[ps.size()-1]+ps[ps.size()-2]);
+        return ps;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
