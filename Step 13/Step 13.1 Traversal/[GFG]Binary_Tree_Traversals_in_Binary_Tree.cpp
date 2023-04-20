ae5f1915c6c7132e9c23d7968931b4f1136e5eae
Priyansh Sharma <77832407+Priyansh-15@users.noreply.github.com>
2023-04-20 17:29:09 +0530

    Update [GFG]Binary_Tree_Traversals_in_Binary_Tree.cpp

--- Step 13/Step 13.1 Traversal/[GFG]Binary_Tree_Traversals_in_Binary_Tree.cpp
+++ Step 13/Step 13.1 Traversal/[GFG]Binary_Tree_Traversals_in_Binary_Tree.cpp
@@ -1,2 +1,25 @@
 // URL : https://practice.geeksforgeeks.org/problems/preorder-traversal/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=preorder-traversal
 
+vector <int> preorder(Node* root)
+{
+  // Your code here
+    vector<int>ans;
+    ans.push_back(root->data);
+    if(root->left)
+    {
+        vector<int>temp=preorder(root->left);
+        for(int i=0;i<temp.size();i++)
+        {
+            ans.push_back(temp[i]);
+        }
+    }
+    if(root->right)
+    {
+        vector<int>temp=preorder(root->right);
+        for(int i=0;i<temp.size();i++)
+        {
+            ans.push_back(temp[i]);
+        }
+    }
+    return ans;
+}
